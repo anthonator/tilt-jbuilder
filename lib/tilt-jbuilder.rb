7a424af00a1d6c4888f408c6e70a63c540fae730
Anthony Smith <anthony@sticksnleaves.com>
2012-06-09 16:26:14 -0400

    First!

---
+++ lib/tilt-jbuilder.rb
@@ -0,0 +1,7 @@
+require "tilt-jbuilder/version"
+
+module Tilt
+  module Jbuilder
+    # Your code goes here...
+  end
+end
---
+++ lib/tilt-jbuilder/version.rb
@@ -0,0 +1,5 @@
+module Tilt
+  module Jbuilder
+    VERSION = "0.0.1"
+  end
+end
