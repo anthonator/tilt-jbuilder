098bf4c3f2e69039d1b4dc5ed6b9cb483755d0fc
Masato Ikeda <masato.ikeda@gmail.com>
2013-03-30 03:06:06 +0900

    Copy :views option in Sinatra to :view_path option

--- lib/sinatra/jbuilder.rb
+++ lib/sinatra/jbuilder.rb
@@ -1,10 +1,18 @@
 require "tilt/jbuilder"
+require "sinatra/base"
 
 module Sinatra
   module Templates
     def jbuilder(template, options={}, locals={})
       render :jbuilder, template, options, locals
     end
+
+    def render_with_view_path_support(engine, data, options = {}, locals = {}, &block)
+      options[:view_path] = options[:views] if engine.to_s.downcase == "jbuilder"
+      render_without_view_path_support(engine, data, options, locals, &block)
+    end
+    alias_method :render_without_view_path_support, :render
+    alias_method :render, :render_with_view_path_support
   end
 end
 
