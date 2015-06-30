9d60f9cbdddbd3421c2b06bbdba1c6c9e8d3af78
Sergey Sein <linchus@gmail.com>
2015-06-30 11:46:20 +0300

    Add support for partial! and array! jbuilder syntax

--- spec/tilt-jbuilder_spec.rb
+++ spec/tilt-jbuilder_spec.rb
@@ -46,13 +46,44 @@ describe Tilt::JbuilderTemplate do
     "{\"author\":\"Anthony\"}".should == template.render
   end
 
-  it "should evaluate partials" do
-    template = Tilt::JbuilderTemplate.new { "json.partial! 'spec/partial', last_name: 'Smith'" }
-    "{\"last_name\":\"Smith\"}".should == template.render
+  describe '.partial!' do
+    it "should evaluate partials" do
+      template = Tilt::JbuilderTemplate.new { "json.partial! 'spec/templates/partial', last_name: 'Smith'" }
+      "{\"last_name\":\"Smith\"}".should == template.render
+    end
+
+    it "should evaluate partials with view_path" do
+      template = Tilt::JbuilderTemplate.new(nil, nil, view_path: 'spec') { "json.partial! '/templates/partial', last_name: 'Smith'" }
+      "{\"last_name\":\"Smith\"}".should == template.render
+    end
+
+    context 'with jbuilder syntax' do
+      let(:template) { Tilt::JbuilderTemplate.new { template_body } }
+      subject { template.render }
+
+      context 'with json.partial! "partial", collection: [], as: :name' do
+        let(:template_body) { "json.partial! 'spec/templates/collection_partial', collection: ['foo', 'bar'], as: :name" }
+        it { is_expected.to eq %q/[{"attribute":"foo"},{"attribute":"bar"}]/ }
+      end
+
+      context 'with json.partial! partial: "partial", collection: [], as: :name' do
+        let(:template_body) { "json.partial! partial: 'spec/templates/collection_partial', collection: ['foo', 'bar'], as: :name" }
+        it { is_expected.to eq %q/[{"attribute":"foo"},{"attribute":"bar"}]/ }
+      end
+
+      context 'with json.partial! partial: "partial", locals: {}' do
+        let(:template_body) { "json.partial! partial: 'spec/templates/partial', locals: { last_name: 'Smith' }" }
+        it { is_expected.to eq %q/{"last_name":"Smith"}/ }
+      end
+    end
   end
 
-  it "should evaluate partials with view_path" do
-    template = Tilt::JbuilderTemplate.new(nil, nil, view_path: 'spec') { "json.partial! '/partial', last_name: 'Smith'" }
-    "{\"last_name\":\"Smith\"}".should == template.render
+  describe '.array!' do
+    let(:template) { Tilt::JbuilderTemplate.new { template_body } }
+    subject { template.render }
+    context 'with json.array! [], partial: "partial", as: :name' do
+      let(:template_body) { "json.array! ['foo', 'bar'], partial: 'spec/templates/collection_partial', as: :name" }
+      it { is_expected.to eq %q/[{"attribute":"foo"},{"attribute":"bar"}]/ }
+    end
   end
 end
